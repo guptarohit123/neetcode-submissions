97161d54f6cc6942d0579b1a2d38bf52e4be323b
guptarohit123 <gupta.rohit123@gmail.com>
2026-09-23 18:18:50 +0200

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] target = new int[26];
+
+
+        for(char c : s1.toCharArray()) {
+            target[ c - 'a']++;
+        }
+
+        int[] window = new int[26];
+
+        int left = 0;
+
+        for(int right = 0; right < s2.length() ; right++) {
+            window[s2.charAt(right) - 'a']++;
+
+            if(right - left + 1 > s1.length()) {
+                window[s2.charAt(left) - 'a']--;
+                left ++;
+            }
+            if(right - left + 1 == s1.length() && Arrays.equals(target,window)) {
+                return true;
+            }
+
+        }
+
+        return false;
+                
+    }
+}
